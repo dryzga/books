a73f9932f4b5eaf982583a9b542054e59c56227f
Mateusz Karwat <mateuszkarwat@gmail.com>
2011-11-21 20:16:17 +0100

    Init commit with cucumber

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.2'
+
+gem 'sqlite3', :group => [:development, :test]
+gem 'pg', :group => [:production]
+
+gem 'rspec-rails', :group => [:development, :test]
+gem 'cucumber-rails', :group => :test
+gem 'database_cleaner', :group => :test
+gem 'webrat'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Books::Application.config.secret_token = '719ab727ad0515429123389cc094749dc5ff4558ef73c8ea69b0e548067d58874c8491d37ddfd29aff754b12a26453363511c86c082b5ed1b9a4a75354b2816a'
