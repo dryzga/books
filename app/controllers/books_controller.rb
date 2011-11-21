2790289d4fa878e175bef5ef38f91c3a5d58095f
Mateusz Karwat <mateuszkarwat@gmail.com>
2011-11-21 22:45:16 +0100

    Second scenario passed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,14 @@ class BooksController < ApplicationController
     @books = Book.all
   end
   
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.create(params[:book])
+    flash[:notice] = "New book added."
+    redirect_to books_path
+  end
+  
 end
