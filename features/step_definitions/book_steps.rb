8a5b42c9df5ed843e017b5a2a13dc4f12688d043
Mateusz Karwat <mateuszkarwat@gmail.com>
2011-11-21 21:59:14 +0100

    Steps for second scenario added

--- features/step_definitions/book_steps.rb
+++ features/step_definitions/book_steps.rb
@@ -11,3 +11,27 @@ end
 Then /^I should see "([^\"]*)"$/ do |text|
   page.should have_content(text)
 end
+
+Given /^I have no books$/ do
+  Book.delete_all
+end
+
+Given /^I am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
+When /^I follow "([^\"]*)"$/ do |link|
+  click_link(link)
+end
+
+When /^I fill in "([^\"]*)" with "([^\"]*)"$/ do |field, value|
+  fill_in(field, :with => value) 
+end
+
+When /^I press "([^\"]*)"$/ do |button|
+  click_button(button)
+end
+
+Then /^I should have ([0-9]+) books?$/ do |count|
+  Book.count.should == count.to_i
+end
